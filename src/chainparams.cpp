0d45aee05853db91c888a2821b6f51c8f7d6f790
mhannigan <michaelhannigan@gmail.com>
2018-05-30 18:18:52 -0400

    merk

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -131,10 +131,10 @@ public:
         genesis.nNonce = 0;
 		
         hashGenesisBlock = genesis.GetHash();
-	printf("%s\n", hashGenesisBlock.ToString().c_str());
+	cdprintf("%s\n", hashGenesisBlock.ToString().c_str());
 	printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
         assert(hashGenesisBlock == uint256("0x0000070ea16c302f5772fd80c9fb5f3e2f39e4982dd29aaf256501f2c474cf6e"));
-        assert(genesis.hashMerkleRoot == uint256("0x990dcbf61f42276c3ee0467902dca262ce517442689cc322cf3b376461bb7cfb"));
+        assert(genesis.hashMerkleRoot == uint256("0x2c11f63d65b3ad04d54e4daf22f6eb0823e6ec70e0814ca4a53600be23af6724"));
         
 
         vSeeds.push_back(CDNSSeedData("nitrous1.brainchamber.com", "nitrous1.brainchamber.com"));
